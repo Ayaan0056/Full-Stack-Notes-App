6fadfa7b9cdea11cbda8db123bc1f9b08fd92873
ayaan dey <ayaan@itobuz.com>
2025-04-23 11:47:51 +0530

    view note function completed

--- NotesApp_frontend/src/components/GetNote.jsx
+++ NotesApp_frontend/src/components/GetNote.jsx
@@ -5,10 +5,10 @@ import { toast, Toaster } from "react-hot-toast";
 import Modal from "./Modal";
 
 const GetNote = ({ isOpen, onClose, noteId, title, content }) => {
-    console.log('onclose: ',onClose)
+    console.log('onclose: ', onClose)
     const fetchNote = async () => {
         const accessToken = localStorage.getItem('accessToken')
-        
+
         try {
             const response = await axios.get(`http://localhost:8000/note/getOneNote/${noteId}`, {
                 headers: {
@@ -16,7 +16,7 @@ const GetNote = ({ isOpen, onClose, noteId, title, content }) => {
                 }
             })
             console.log('response: ', response)
-            
+
         }
         catch (error) {
             console.log("error occured: ", error)
@@ -24,37 +24,37 @@ const GetNote = ({ isOpen, onClose, noteId, title, content }) => {
         }
     }
 
-    if(isOpen){
+    if (isOpen) {
         fetchNote()
     }
 
     const GetModal = ({ isOpen, onClose }) => {
         return (
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <div className="flex flex-col gap-5">
-              <h2 className="text-lg font-bold">Note</h2>
-              <form action="#">
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <div className="flex flex-col gap-5">
-                  <input
-                    type="text"
-                    className="ps-2 min-w-[28vw] min-h-[5vh]"
-                    defaultValue={title}
-                    readOnly
-                  />
-    
-                  <textarea
-                    aria-label="Post Content"
-                    type="text"
-                    className="min-h-[20vh] min-w-[28vw] p-5 placeholder:text-2xl text-2xl"
-                    defaultValue={content}
-                    readOnly
-                  />
+                    <h2 className="text-lg font-bold">Note</h2>
+                    <form action="#">
+                        <div className="flex flex-col gap-5">
+                            <input
+                                type="text"
+                                className="ps-2 min-w-[28vw] min-h-[5vh]"
+                                defaultValue={title}
+                                readOnly
+                            />
+
+                            <textarea
+                                aria-label="Post Content"
+                                type="text"
+                                className="min-h-[20vh] min-w-[28vw] p-5 placeholder:text-2xl text-2xl"
+                                defaultValue={content}
+                                readOnly
+                            />
+                        </div>
+                    </form>
                 </div>
-              </form>
-            </div>
-          </Modal>
+            </Modal>
         );
-      };
+    };
 
     return (
         <>
